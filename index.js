2a8d2094169dc2b61b50442744f0e7aefad5c892
サム <118713201+bowdensl712@users.noreply.github.com>
2022-11-23 14:02:00 +0900

    追加： index.html, index.js, styles.css

---
+++ index.js
@@ -0,0 +1,144 @@
+/* HTML Elements */
+const container = document.getElementById('container');
+let title = document.createElement("h1");
+let text1 = document.createElement("p");
+let button1 = document.createElement("button");
+let text2 = document.createElement("p");
+let playerStats = document.createElement("p");
+let enemyStats = document.createElement("p");
+let button2 = document.createElement("button");
+button2.setAttribute("onclick", "combat()");
+button2.innerText = "Attack";
+let playerDamage = document.createElement("p");
+let enemyDamage = document.createElement("p");
+let text3 = document.createElement("p");
+
+/* Game Stats */
+let name;
+let hp = 100;
+let attackMult = 1;
+let defenseMult = 1;
+let roomNum = 1;
+let currentEnemy = ["TEST", 10, 1, 1, "test description"];
+
+/* Room Descriptions */
+const roomType = ["dungeon", "cave"];
+let roomTypeDuration = 0;
+let currentRoomType;
+const descOpen = ["You stand in", "You find yourself in", "This is", "You enter", "You step into", "You arrive at", "It's", "You're faced with", "You see before you"];
+/* Generic Dungeon */
+const dungeonAdj = ["a dank", "a dark", "a gloomy", "a moldy old", "a long unused", "a long abandoned", "a cavernous", "a surprisingly well-preserved", "an old, decaying", "a dark and gloomy", "a barren", "an unremarkable"];
+const dungeonNoun = ["stone room", "chamber", "area", "crypt", "catacomb", "room"];
+const dungeonExtra = ["with bloodstains on the wall, seemingly from many years ago", "with old, rusted contraptions scattered about", "with round wooden tables and chairs, arranged like a dining area", "with old rusted cages, some with bones remaining inside", "with no furnishings that you can see", "bare walls, and only bits of stone and brick littering the floor", "with torches lining the walls, illuminating the space", "with solid wooden doors lining the walls, sealed with sturdy iron locks"];
+
+/* Cave */
+const caveAdj = ["a dank", "a dark", "a gloomy", "a naturally formed", "a long unused", "an expansive", "a very dark", "a creepy", "a surprisingly beautiful", "a dark and imposing", "a narrow", "a spacious and expansive", "a cramped"];
+const caveNoun = ["cave", "cave", "cave", "cave corridor", "cavern", "section of cave", "tunnel of stone", "stone cavern"];
+const caveExtra = ["with the occasional sound of dripping water", "with long stalactites hanging from above", "with small stalagmites creating a bumpy texture to the stone below your feet", "with a small pool of water to one side"];
+
+
+/* Enemies [Name,  HP, Attack, Defense, Description]*/
+dungeonEnemies = [
+    ["Frail skeleton", 10, 0.7, 0.5, "A small skeleton with thin, fragile bones. It wields a sharpened femur, and is ready to fight"], 
+    ["Young goblin", 15, 1, 0.7, "A small goblin, barely past maturity. While their size is small, they wield a razor-sharp spear that's more than capable of ending a human life."]
+];
+
+caveEnemies = [
+    ["Cave slime", 20, 0.2, 2.0, "A large, robust slime. It has a murky, pale color, but seems to sparkle in the light, like a glistening pool hidden deep under the earth."],
+    ["Stone spider", 10, 1.5, 0.4, "A giant spider with long, spindly legs made of stone. Its venom is capable of causing immense injury and death, but it fragile limbs make it rather weak to attacks."]
+]
+
+
+/* Functions */
+
+function random(x) {
+    return x[Math.floor(Math.random() * x.length)];
+};
+
+function selectRoomType() {
+    roomTypeDuration = Math.floor(Math.random() * 10);
+    currentRoomType = random(roomType);
+};
+
+function roomDesc() {
+    if (currentRoomType === "dungeon") {
+        text1.innerText = random(descOpen) + ' ' + random(dungeonAdj) + ' ' + random(dungeonNoun) + ', ' + random(dungeonExtra) + '.'
+    }
+    else if (currentRoomType === "cave") {
+        text1.innerText = random(descOpen) + ' ' + random(caveAdj) + ' ' + random(caveNoun) + ', ' + random(caveExtra) + '.'
+    }
+    else {console.error("roomDesc() isn't working correctly!")}
+}
+
+
+
+
+
+/* Opening Screen */
+
+title.innerText = "Dungeon Crawler";
+text1.innerText = "You are a fledgling adventurer, about to step foot into the endless dungeon of doom. \n\n You will not survive this journey, but perhaps you will find glory before your doom. \n\nAre you ready?";
+button1.innerText = "I was born for this.";
+button1.setAttribute('onclick', 'startGame()');
+playerStats.innerText = "\nPlayer:\n" + "HP: " + hp + "\nDamage: x" + attackMult + "\nDefense: x" + defenseMult;
+container.append(title, text1, button1, playerStats);
+
+
+
+
+
+ 
+
+/* Basic Game Loop */
+function startGame() {
+    button1.innerText = "Continue forward";
+    title.innerText = "Room " + roomNum;
+    roomNum ++;
+    if (roomTypeDuration < 1) {selectRoomType()};
+    roomTypeDuration --;
+    roomDesc();
+    selectEnemy();
+   
+};
+
+/* Selects an enemy based on room type*/
+function selectEnemy() {
+    spawnChance = Math.random();
+    if (currentRoomType === "dungeon" && spawnChance > .5) {
+        currentEnemy = random(dungeonEnemies);
+        spawnEnemy();
+    }
+    else if (currentRoomType === "cave" && spawnChance > .5) {
+        currentEnemy = random(caveEnemies);
+        spawnEnemy();
+    }
+    console.log(spawnChance);
+};
+
+/* Spawns the enemy */
+function spawnEnemy() {
+    button1.remove();
+    playerStats.insertAdjacentElement("beforebegin", text2);
+    playerStats.insertAdjacentElement("beforebegin", button2);
+    playerStats.insertAdjacentElement("beforebegin", enemyStats);
+    text2.innerText = "\nSuddenly, you see a " + currentEnemy[0].toLowerCase() + ". \n Description:\n" + currentEnemy[4];
+    enemyStats.innerText = "\n" + currentEnemy[0] + ":\n" + "HP: " + currentEnemy[1] + "\nDamage: x" + currentEnemy[2] + "\nDefense: x" + currentEnemy[3];
+};
+/*TODO: Add a clearEnemy() function to remove the enemy description and return button1 upon defeat */
+
+
+function combat() {
+    playerAttack = (Math.floor(Math.random() * 11) * attackMult) / currentEnemy[3];
+    playerStats.insertAdjacentElement("afterend", playerDamage);
+    if (currentEnemy[1] < 1) {
+        text3.innerText = "You defeated " + currentEnemy;
+        playerStats.insertAdjacentElement("afterend", text3);
+        clearEnemy();
+    }
+    enemyAttack = (Math.floor(Math.random() * 11) * currentEnemy[2]) / defenseMult;
+    playerStats.insertAdjacentElement("afterend", enemyDamage);
+    
+};
+
+
+
